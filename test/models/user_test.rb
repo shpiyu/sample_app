6ef4265bc9019e989e198461e5c8307840b989ce
Piyush Ranjan <piyushranjan95@gmail.com>
2017-02-06 06:26:48 +0530

    add presence and length validations

---
+++ test/models/user_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+  def setup
+    @user = User.new(email: "amazingpanda@gmail.com",  name: "Al karta")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "name should be present" do
+    @user.name = "    "
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = "      "
+    assert_not @user.valid?
+  end
+
+  test "name should not be too long" do
+    @user.name = "A"*51
+    assert_not @user.valid?
+  end
+
+  test "email should not be too long" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid? 
+  end
+end
